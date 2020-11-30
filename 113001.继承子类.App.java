400cec63da55857dc84308c806f7cd3bb04de7c3
2505984231 <50291288+2505984231@users.noreply.github.com>
2020-11-30 12:02:20 +0800

    Update 113001.继承子类.App.java

--- 113001.继承子类.App.java
+++ 113001.继承子类.App.java
@@ -1,17 +1,8 @@
 package pac;
 
-public class R {
-	static void Test() {
-		String boy_filePath="img/boy.gif";
-		String girl_filePath="img/girl.gif";
-		int [][]arr={{1,2,3},{4,5,6}};
-		System.out.println("boy_filePath:"+boy_filePath);
-		System.out.println("girl_filePath:"+girl_filePath);
-		for (int row=0;row<arr.length;row++) {
-			for(int column=0;column<arr[0].length;column++) {
-				System.out.print(arr[row][column]);
-			}
-			System.out.println();
-		}
-	}		
+public class App extends R{
+
+	public static void main(String[] args) {
+		Test();
+	}
 }
