022fac40f40e97b18a0c5a5f7d0f5b4977916090
2505984231 <50291288+2505984231@users.noreply.github.com>
2020-12-08 18:08:58 +0800

    Update 120701.寻找0的相邻坐标.Find.java

--- 120701.寻找0的相邻坐标.Find.java
+++ 120701.寻找0的相邻坐标.Find.java
@@ -1,49 +1,54 @@
-package pac;
-
+  
+package packages;
+	
 public class Find {
- void findZero(int array2d[][]) {
-	 for(int y=0;y<array2d.length;y++) {
-		 for(int x=0;x<array2d[0].length;x++) {
-			 if(array2d[y][x]==0) {
-				 System.out.println("坐标是：R_zero("+x+","+y+")");
-			 }
-		 }
-	 }
- }
- void findLeft(int array2d[][]) {
-	 for(int y=3;y<array2d.length;y++) {
-		 for(int x=0;x<array2d[1].length;x++) {
-			 if(array2d[y][x]==10) {
-				 System.out.println("坐标是：R_left("+x+","+y+")");
-			 }
-		 }
-	 }
- }
- void findRight(int array2d[][]) {
-	 for(int y=0;y<array2d.length;y++) {
-		 for(int x=0;x<array2d[0].length;x++) {
-			 if(array2d[y][x]==12) {
-				 System.out.println("坐标是：R_right("+x+","+y+")");
-			 }
-		 }
-	 }
- }
- void findUp(int array2d[][]) {
-	 for(int y=0;y<array2d.length;y++) {
-		 for(int x=0;x<array2d[0].length;x++) {
-			 if(array2d[y][x]==5) {
-				 System.out.println("坐标是：R_up("+x+","+y+")");
-			 }
-		 }
-	 }
- }
- void findDown(int array2d[][]) {
-	 for(int y=0;y<array2d.length;y++) {
-		 for(int x=0;x<array2d[0].length;x++) {
-			 if(array2d[y][x]==11) {
-				 System.out.println("坐标是：R_down("+x+","+y+")");
-			 }
-		 }
-	 }
- }
+	void	findZERO(int array2d[][]){
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				if (array2d[row][column]==0) {
+					System.out.println("R_zero("+column+","+row+")");
+				}
+			}
+		}
+	}
+	void	findleft(int array2d[][]){
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				if (array2d[row][column]==0) {
+					column=column-1;
+					System.out.println("R_left("+column+","+row+")");return;
+				}
+			}
+		}
+	}
+	void	findright(int array2d[][]){
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				if (array2d[row][column]==0) {
+					column++;
+					System.out.println("R_right("+column+","+row+")");
+				}
+			}
+		}
+	}
+	void	findup(int array2d[][]){
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				if (array2d[row][column]==0) {
+					row--;
+					System.out.println("R_up("+column+","+row+")");return;
+				}
+			}
+		}
+	}
+	void	finddown(int array2d[][]){
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				if (array2d[row][column]==0) {
+					row++;
+					System.out.println("R_down("+column+","+row+")");
+				}
+			}
+		}
+	}
 }
