6dd0b43e0bd7aac2cf7b91ce4192e560732f6698
2505984231 <50291288+2505984231@users.noreply.github.com>
2020-12-15 23:45:29 +0800

    Update 121402.奇数求和.App.java

--- 121402.奇数求和.App.java
+++ 121402.奇数求和.App.java
@@ -1,1 +1,14 @@
+package pac;
 
+import java.util.Scanner;
+
+public class App {
+	public static int main(String[] args) {
+		long i = 1;
+    long sum = 0;
+    while (i <= 2147483647){
+        if (i % 2 == 1){
+            sum = sum + i;
+        }
+        i++;
+    }System.out.println("sum = "+sum);}}
